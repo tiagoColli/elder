3d578565c8a81a71a240c204dde20ca8d5548973
Tiago Colli <tc.plakitca@gmail.com>
2026-02-24 22:04:30 -0300

    feat: add CI pipeline with parallel quality gates (format, credo, dialyzer, sobelow, test)

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Elder.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_local_path: "priv/plts", plt_core_path: "priv/plts"]
     ]
   end
 
@@ -21,8 +22,9 @@ defmodule Elder.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
     ]
   end
 end
